1c2bb62a0275669946661f9836b11ebe56872cab
tonKristall <krisale@yandex.ru>
2021-01-28 00:56:18 +0300

    feat: test pass 17 ms

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(ar) {
+  if (ar == undefined || ar.length == 0) {
+    return 0;
+  }
+  let min = ar[0];
+  ar.forEach(element => {
+    if (element < min) {
+      min = element;
+    }
+  })
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(ar) {
+  if (ar == undefined || ar.length == 0) {
+    return 0;
+  }
+  let max = ar[0];
+  ar.forEach(element => {
+    if (element > max) {
+      max = element;
+    }
+  })
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(ar) {
+  if (ar == undefined || ar.length == 0) {
+    return 0;
+  }
+  let s = 0;
+  let avg = 0;
+  ar.forEach(element => {
+    s += element;
+  })
+  avg = s / ar.length;
+  return avg;
 }
